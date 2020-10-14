c11ea6351555522927b010223bf37450ef9e01fd
Breno M <breno_marino@hotmail.com>
2020-10-14 17:57:51 -0300

    project start

---
+++ src/main/java/br/com/tijo/api/mail/ApiMailApplication.java
@@ -0,0 +1,13 @@
+package br.com.tijo.api.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMailApplication.class, args);
+	}
+
+}
