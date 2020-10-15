fa1aaaec419051bf17ef4807363d6c68f6f69f8a
Breno M <breno_marino@hotmail.com>
2020-10-15 02:16:36 -0300

    wip: copia dos ajustes do projeto com grandle

---
+++ src/main/java/br/com/tijo/api/mail/controller/EmailController.java
@@ -0,0 +1,29 @@
+package br.com.tijo.api.mail.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tijo.api.mail.dto.EmailRequestDTO;
+import br.com.tijo.api.mail.service.EmailService;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+	private final EmailService emailService;
+	
+	public EmailController(EmailService emailService) {
+		this.emailService = emailService;
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public String sendEmailToResetPassword(@RequestBody EmailRequestDTO dtoRequest) {
+		return emailService
+				.sendEmailToResetPassword(dtoRequest);
+	} 
+}
---
+++ src/main/java/br/com/tijo/api/mail/service/EmailService.java
@@ -0,0 +1,58 @@
+package br.com.tijo.api.mail.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import br.com.tijo.api.mail.dto.EmailRequestDTO;
+
+@Service
+public class EmailService {
+	
+	private final JavaMailSender mailSender;
+	private final TemplateEngine templateEngine;
+	
+	public EmailService (JavaMailSender mailSender, TemplateEngine templateEngine) {
+		this.mailSender = mailSender;
+		this.templateEngine = templateEngine;
+	}
+
+	public String sendEmailToResetPassword(EmailRequestDTO emailRequestDTO){
+		try {
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	        
+	        Context context = new Context();
+	
+	        context.setVariable("name", emailRequestDTO.getUserName());
+	        context.setVariable("psw", emailRequestDTO.getTempPsw());
+	        
+	        String body = templateEngine.process("ResetMailTemplate", context);
+	        
+	        helper.setFrom(new InternetAddress("contato@tijo.com.br"));
+	        helper.setTo(emailRequestDTO.getEmail());
+	        helper.setSubject("Reset de Senha Tijo	");
+	        helper.setText(body, true);
+
+            mailSender.send(message);
+            
+            return "Email enviado com sucesso!";
+		}catch(MessagingException messaginExecption) {
+			messaginExecption.printStackTrace();
+            return messaginExecption.getLocalizedMessage();
+        }catch(MailException emailException) { 
+        	emailException.printStackTrace();
+            return emailException.getLocalizedMessage();
+	    }catch (Exception genericException) {
+	    	genericException.printStackTrace();
+            return "Erro ao enviar email.";
+        }
+	}
+}
