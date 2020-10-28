ef5831041d46dce6bd3d8370120a530b5f81a944
Breno Marino de Souza Ferreira <breno_marino@hotmail.com>
2020-10-28 13:32:25 -0300

    mais alteracoes

--- src/main/java/br/com/tijo/api/mail/ApiMailApplication.java
+++ src/main/java/br/com/tijo/api/mail/ApiMailApplication.java
@@ -2,8 +2,12 @@ package br.com.tijo.api.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"br.com.tijo.api"})
 public class ApiMailApplication {
 
 	public static void main(String[] args) {
